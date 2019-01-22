fdf400c95b9ba0fa726b2d7d544b74b748787e70
mapktah <mark.tan@Administrators-MacBook-Pro.local>
2019-01-22 14:21:05 +0800

    updates

--- mozg/examples/Keras_Example.R
+++ mozg/examples/Keras_Example.R
@@ -1,3 +1,22 @@
 library(keras)
 
 mnist <- dataset_mnist()
+
+train_images <- mnist$train$x
+train_labels <- mnist$train$y
+test_images <- mnist$test$x
+test_labels <- mnist$test$y
+
+str(train_images)
+str(train_labels)
+
+network <- keras_model_sequential() %>%
+  layer_dense(
+    units = 512,
+    activation = "relu",
+    input_shape = c(28 * 28)
+  ) %>%
+  layer_dense(
+    units = 10,
+    activation = "softmax"
+  )
