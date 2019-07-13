6fb6be58e4c99ee823e54d3e54cdd2cb1bc135af
mapktah <mapktah@yandex.ru>
2019-07-13 16:38:54 +0800

    remove np matrix replace with np array

--- src/mozg/lib/math/Cluster.py
+++ src/mozg/lib/math/Cluster.py
@@ -6,6 +6,8 @@
 import numpy as np
 import pandas as pd
 from scipy.cluster.vq import vq, kmeans, whiten
+import mozg.common.util.Log as lg
+from inspect import currentframe, getframeinfo
 
 
 class Cluster:
@@ -21,22 +23,43 @@ class Cluster:
     # TODO: Otherwise this simple method below works but quite badly.
     #
     @staticmethod
-    def get_optimal_cluster(matx, n_tries, iterations=50, threshold_change=THRESHOLD_CHANGE_DIST_TO_CENTROIDS, verbose=0):
-
+    def get_optimal_cluster(
+            matx,
+            n_tries,
+            iterations       = 50,
+            threshold_change = THRESHOLD_CHANGE_DIST_TO_CENTROIDS
+    ):
+        lg.Log.debug(
+            str(Cluster.__name__) + ' ' + str(getframeinfo(currentframe()).lineno)
+            + ': Calculating optimal cluster for data\n\r' + str(matx) + '.'
+        )
         prev_sum_dist_to_centroids = 99999999.0
-        prev_ratio_n_points_n_clusters = 99999999.0
+        points_per_cluster_prev = 99999999.0
         optimal_clusters = 1
         #
         # We try different cluster counts starting from 1 cluster
         #
         for i in range(1, n_tries+1, 1):
+            lg.Log.debug(
+                str(Cluster.__name__) + ' ' + str(getframeinfo(currentframe()).lineno)
+                + ': Trying ' + str(i) + ' cluster(s)...'
+            )
             # Cluster points in <i> clusters
             i_cluster_kmeans = kmeans(matx, k_or_guess=i, iter=iterations)
+            # Code book when assigning a cluster back to the samples
             i_cluster_idx = vq(obs=matx, code_book=i_cluster_kmeans[0])
+            lg.Log.debug(
+                str(Cluster.__name__) + ' ' + str(getframeinfo(currentframe()).lineno)
+                + '\n\r: Kmeans \n\r' + str(i_cluster_kmeans)
+                + '\n\r, Indexes \n\r' + str(i_cluster_idx) + '.'
+            )
 
-            ratio_n_points_n_clusters = matx.shape[0] / i
-            change_ratio_n_points_n_clusters = ((ratio_n_points_n_clusters - prev_ratio_n_points_n_clusters)
-                                                / prev_ratio_n_points_n_clusters)
+            #
+            # How many points per cluster on average
+            #
+            points_per_cluster = matx.shape[0] / i
+            rate_of_change_points_per_cluster =\
+                ((points_per_cluster - points_per_cluster_prev) / points_per_cluster_prev)
 
             #
             # Get sum of mean squares of distances of all points to their respective centroids
@@ -47,23 +70,39 @@ class Cluster:
             for j in range(0, matx.shape[0], 1):
                 j_point_vec = matx[j]
                 j_point_cluster_no = i_cluster_idx[0][j]
-                j_point_centroid = np.matrix(i_cluster_kmeans[0][j_point_cluster_no])
+                j_point_centroid = np.array(i_cluster_kmeans[0][j_point_cluster_no], ndmin=2)
+                # If this is a column matrix, with >1 rows, and a single column, transpose to become a row matrix
+                if (j_point_centroid.shape[0] > 1) and (j_point_centroid.shape[1] == 1):
+                    j_point_centroid = j_point_centroid.transpose()
                 tmp = j_point_vec - j_point_centroid
                 j_distance_to_centroid = tmp*tmp.transpose()
                 j_distance_to_centroid = j_distance_to_centroid[0,0] ** 0.5
                 i_sum_dist_to_centroids = i_sum_dist_to_centroids + j_distance_to_centroid
                 n = n + 1
+                lg.Log.debugdebug(
+                    str(Cluster.__name__) + ' ' + str(getframeinfo(currentframe()).lineno)
+                    + ': Sample/Point ' + str(np.round(j_point_vec,3)) + ' in cluster ' + str(j_point_cluster_no)
+                    + ', centroid ' + str(np.round(j_point_centroid,3))
+                    + ', Distance = ' + str(round(j_distance_to_centroid,3))
+                    + ', sum distance = ' + str(round(i_sum_dist_to_centroids,3)) + '.'
+                )
 
+            lg.Log.debugdebug(
+                str(Cluster.__name__) + ' ' + str(getframeinfo(currentframe()).lineno)
+                + ': ' + str(i) + ' clusters. Sum distance to centroid = ' + str(i_sum_dist_to_centroids) + '.'
+            )
             # Change (reduction) of sum of mean square of distances
-            i_sum_dist_to_centroids = i_sum_dist_to_centroids
-            change_sum_mean_square = ((i_sum_dist_to_centroids - prev_sum_dist_to_centroids) / prev_sum_dist_to_centroids)
+            reduction_pct_sum_mean_square =\
+                - ((i_sum_dist_to_centroids - prev_sum_dist_to_centroids) / prev_sum_dist_to_centroids)
             avg_dist_to_centroids = i_sum_dist_to_centroids / n
             optimal_clusters = i
 
             #
             # Calculate ratio of closest distance between centroids to furthest distance between point-centroid
             #
+            # Ideally "far"
             closest_dist_btw_centroids = 99999999
+            # Ideally "well separated"
             avg_dist_btw_centroids = 0
             n = 0
             for j in range(0, i, 1):
@@ -76,17 +115,27 @@ class Cluster:
                     n = n + 1
             if n>0:
                 avg_dist_btw_centroids = avg_dist_btw_centroids / n
-            if verbose>=1:
-                print('Clusters ' + str(i) + ', change = ' + str(round(change_sum_mean_square*100,2)) + '%.' +
-                      #'Closest distance between centroids = ' + str(round(closest_dist_btw_centroids, 2)) + '.' +
-                      ' Avg dist btw centroids = ' + str(round(avg_dist_btw_centroids, 2)) + '.' +
-                      ' Avg dist to centroids = ' + str(round(avg_dist_to_centroids, 2)) + '. ' +
-                      ' Point to cluster ratio = ' + str(round(ratio_n_points_n_clusters, 2)) +
-                      ' (' + str(round(change_ratio_n_points_n_clusters*100, 2)) + '% change).')
-            if change_sum_mean_square > -threshold_change:
+            lg.Log.debugdebug(
+                str(Cluster.__name__) + ' ' + str(getframeinfo(currentframe()).lineno)
+                + '\n\r' + str(i) + ' Clusters'
+                + '\n\r\tReduction % of sum mean square (from ' + str(i-1) + ' clusters) = '
+                + str(round(reduction_pct_sum_mean_square*100,2)) + '%.'
+                + '\n\r\tAvg dist btw centroids = ' + str(round(avg_dist_btw_centroids, 2)) + '.'
+                + '\n\r\tClosest dist btw centroids = ' + str(round(closest_dist_btw_centroids, 2)) + '.'
+                + '\n\r\tAvg dist to centroids = ' + str(round(avg_dist_to_centroids, 2)) + '. '
+                + '\n\r\tPoints per cluster = ' + str(round(points_per_cluster, 2))
+                + ' (' + str(round(rate_of_change_points_per_cluster*100, 2)) + '% change).')
+
+            #
+            # Simple Criteria for Optimal Cluster Count
+            # - If the change of the sum mean square is becoming too small, means we are already optimal
+            # TODO Add criteria such that average distance between centroids don't become too "close"
+            # TODO Add criteria such that closest distance between centroids is "much bigger" than avg distance to centroids
+            #
+            if reduction_pct_sum_mean_square < threshold_change:
                 break
             prev_sum_dist_to_centroids = i_sum_dist_to_centroids
-            prev_ratio_n_points_n_clusters = ratio_n_points_n_clusters
+            points_per_cluster_prev = points_per_cluster
 
         return optimal_clusters
 
@@ -94,16 +143,19 @@ class Cluster:
     # The main clustering function
     #
     @staticmethod
-    def cluster(matx,
-                feature_names,
-                ncenters,
-                iterations=50,
-                verbose=0):
+    def cluster(
+            matx,
+            feature_names,
+            ncenters,
+            iterations=50
+    ):
+        lg.Log.debug(
+            str(Cluster.__name__) + ' ' + str(getframeinfo(currentframe()).lineno)
+            + ': Start clustering ncenters=' + str(ncenters) + ', data=\n\r' + str(matx)
+        )
         # Set starting centers to be the top keywords
         ncols_matx = matx.shape[1]
-        centers_initial = np.zeros((ncenters, ncols_matx))
-        #for i in range(0, ncenters, 1):
-        #    centers_initial[i][i] = 1
+        # centers_initial = np.zeros((ncenters, ncols_matx))
         cluster_kmeans = kmeans(matx, k_or_guess=ncenters, iter=iterations)
         cluster_idx = vq(obs=matx, code_book=cluster_kmeans[0])
 
@@ -112,34 +164,48 @@ class Cluster:
         #
         nrows = cluster_kmeans[0].shape[0]
         df_cluster_matrix = pd.DataFrame(data=cluster_kmeans[0], columns=feature_names, index=list(range(0, nrows, 1)))
-        #print(df_cluster_matrix)
+        lg.Log.debugdebug(
+            str(Cluster.__name__) + ' ' + str(getframeinfo(currentframe()).lineno)
+            + ': Cluster matrix\n\r' + str(df_cluster_matrix)
+        )
 
         point_cluster_no = [0]*matx.shape[0]
         point_distance_to_center = [0]*matx.shape[0]
         # Assign cluster number to lines
-        if verbose >= 1:
-            print('Assigning clusters to ' + str(matx.shape[0]) + ' sentences')
+        lg.Log.info(
+            str(Cluster.__name__) + ' ' + str(getframeinfo(currentframe()).lineno)
+            + ': Assigning clusters to ' + str(matx.shape[0]) + ' sentences...'
+        )
         for i in range(0, matx.shape[0], 1):
-            point_vec = matx[i]
+            point_vec = np.array(matx[i], ndmin=1)
+            lg.Log.debugdebug('point vec: ' + str(point_vec))
             cluster_no = cluster_idx[0][i]
             point_cluster_no[i] = cluster_no
 
-            point_centroid = np.matrix(cluster_kmeans[0][cluster_no])
+            point_centroid = np.array(cluster_kmeans[0][cluster_no], ndmin=1)
+            lg.Log.debugdebug('point centroid: ' + str(point_centroid))
+
             tmp = point_vec - point_centroid
-            distance_to_center = tmp*tmp.transpose()
-            point_distance_to_center[i] = distance_to_center[0,0]**0.5
+            lg.Log.debugdebug('Dif = ' + str(tmp))
+            distance_to_center = np.sum(np.multiply(tmp,tmp))
+            point_distance_to_center[i] = distance_to_center**0.5
 
         # The cluster number of each point, in the same order as the original matrix
         df_point_cluster = pd.DataFrame({
-                                        'ClusterNo': point_cluster_no,
-                                        'DistanceToCenter': point_distance_to_center
-                                        })
+            'ClusterNo': point_cluster_no,
+            'DistanceToCenter': point_distance_to_center
+        })
+        lg.Log.info(
+            str(Cluster.__name__) + ' ' + str(getframeinfo(currentframe()).lineno)
+            + ': Final Cluster\n\r ' + str(df_point_cluster)
+        )
         return { 'ClusterMatrix': df_cluster_matrix, 'PointClusterInfo': df_point_cluster }
 
 
-def demo_1():
+if __name__ == '__main__':
+    lg.Log.LOGLEVEL = lg.Log.LOG_LEVEL_DEBUG_2
     fn = ['a', 'b', 'c', 'd', 'e']
-    m = np.matrix(data=np.zeros((8, 5)))
+    m = np.zeros((8, 5))
     m[0] = [1,2,1,0,0]
     m[1] = [2,1,2,0,0]
     m[2] = [1,1,1,0,0]
@@ -152,18 +218,16 @@ def demo_1():
     # Optimal clusters
     optimal_clusters = Cluster.get_optimal_cluster(
         matx=m,
-        n_tries=m.shape[0],
-        verbose=1)
+        n_tries=m.shape[0]
+    )
     print('Optimal Clusters = ' + str(optimal_clusters))
 
-    retval = Cluster.cluster(matx=m,
-                             feature_names=fn,
-                             ncenters=3,
-                             iterations=20,
-                             verbose=1)
-    print(retval['ClusterMatrix'])
-    print(retval['PointClusterInfo'])
-    return
-
-
-#demo_1()
+    retval = Cluster.cluster(
+        matx=m,
+        feature_names=fn,
+        ncenters=3,
+        iterations=20
+    )
+    #print(retval['ClusterMatrix'])
+    #print(retval['PointClusterInfo'])
+    exit(0)
