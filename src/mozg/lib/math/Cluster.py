5d605f442713cc1ceaf7ffdf9512332e77d793d3
mapktah <mapktah@yandex.ru>
2019-07-21 17:37:11 +0800

    improvements

--- src/mozg/lib/math/Cluster.py
+++ src/mozg/lib/math/Cluster.py
@@ -9,7 +9,13 @@ from scipy.cluster.vq import vq, kmeans, whiten
 import mozg.common.util.Log as lg
 from inspect import currentframe, getframeinfo
 
-
+#
+# Some concepts:
+#
+# 1. Cluster Radius
+#    For all classes, or cluster the radius of the class/cluster is defined as the distance of the
+#    "center" of the class/cluster to the furthest point.
+#
 class Cluster:
 
     def __init__(self):
@@ -17,9 +23,23 @@ class Cluster:
 
     THRESHOLD_CHANGE_DIST_TO_CENTROIDS = 0.1
 
-    COL_CLUSTER_MATRIX = 'ClusterMatrix'
-    COL_CLUSTER_NDARRY = 'ClusterNdarray'
-    COL_CODE_BOOK      = 'PointClusterInfo'
+    COL_CLUSTER_LABEL = 'ClusterLabel'
+    COL_CLUSTER_RADIUS = 'ClusterRadius'
+
+    #
+    # The return object for our cluster function
+    #
+    class ReturnCluster:
+        def __init__(
+                self,
+                np_cluster_centers,
+                np_cluster_labels,
+                df_distances_point_to_cc
+        ):
+            self.np_cluster_centers = np_cluster_centers
+            self.np_cluster_labels = np_cluster_labels,
+            self.df_distances_point_to_cc = df_distances_point_to_cc
+            return
 
     #
     # Get's optimal clusters, based on changes on sum of mean-square distance to centroids.
@@ -173,57 +193,81 @@ class Cluster:
         # centers_initial = np.zeros((ncenters, ncols_matx))
         cluster_kmeans = kmeans(matx, k_or_guess=ncenters, iter=iterations)
         cluster_idx = vq(obs=matx, code_book=cluster_kmeans[0])
-
-        #
-        # Keep cluster matrix in data frame
-        #
-        nrows = cluster_kmeans[0].shape[0]
-        np_cluster = np.array(cluster_kmeans[0])
-        df_cluster_matrix = pd.DataFrame(data=cluster_kmeans[0], columns=feature_names, index=list(range(0, nrows, 1)))
         lg.Log.debugdebug(
-            str(Cluster.__name__) + ' ' + str(getframeinfo(currentframe()).lineno)
-            + ': Cluster matrix\n\r' + str(df_cluster_matrix)
+            'Cluster kmeans:\n\r' + str(cluster_kmeans)
+            + '\n\r, and cluster vq:\n\r' + str(cluster_idx)
         )
 
-        point_cluster_no = [0]*matx.shape[0]
-        point_distance_to_center = [0]*matx.shape[0]
-        # Assign cluster number to lines
-        lg.Log.info(
-            str(Cluster.__name__) + ' ' + str(getframeinfo(currentframe()).lineno)
-            + ': Assigning clusters to ' + str(matx.shape[0]) + ' sentences...'
+        np_cluster_centers= np.array(cluster_kmeans[0])
+        np_cluster_labels = np.array(cluster_idx[0])
+        np_unique_cluster_labels = np.array(list(set(np_cluster_labels)))
+
+        # Each row represents the points (indexes only), and columns the distance to the respective column center
+        df_dist_to_cluster_centers = pd.DataFrame(
+            data = np.zeros(shape=(matx.shape[0], np_unique_cluster_labels.shape[0])),
+            columns = np_unique_cluster_labels,
+            index   = range(matx.shape[0])
         )
-        for i in range(0, matx.shape[0], 1):
-            point_vec = np.array(matx[i], ndmin=1)
-            lg.Log.debugdebug('point vec: ' + str(point_vec))
-            cluster_no = cluster_idx[0][i]
-            point_cluster_no[i] = cluster_no
+        df_dist_to_cluster_centers = df_dist_to_cluster_centers.transpose()
 
-            point_centroid = np.array(cluster_kmeans[0][cluster_no], ndmin=1)
-            lg.Log.debugdebug('point centroid: ' + str(point_centroid))
+        # Get distance of all points to all cluster centers
+        for idx in np_unique_cluster_labels:
+            cc = np_cluster_centers[idx]
+            #print('cc: ' + str(cc))
+            np_cc = np.array([cc]*matx.shape[0])
+            #print('np_cc:\n\r' + str(np_cc))
+            dist = matx - np_cc
+            # Square every array element
+            dist = np.power(dist, 2)
+            # Sum every row to create a single column matrix
+            dist = dist.sum(axis=1)
+            # Take the square root of every element in the single column matrix as distance
+            dist = np.power(dist, 0.5)
+            # Convert to a row matrix
+            dist = dist.transpose()
+            print('dist:' + str(dist))
+            df_dist_to_cluster_centers.at[idx] = dist
 
-            tmp = point_vec - point_centroid
-            lg.Log.debugdebug('Dif = ' + str(tmp))
-            distance_to_center = np.sum(np.multiply(tmp,tmp))
-            point_distance_to_center[i] = distance_to_center**0.5
-
-        # The cluster number of each point, in the same order as the original matrix
-        df_point_cluster = pd.DataFrame({
-            'ClusterNo': point_cluster_no,
-            'DistanceToCenter': point_distance_to_center
-        })
-        lg.Log.info(
-            str(Cluster.__name__) + ' ' + str(getframeinfo(currentframe()).lineno)
-            + ': Final Cluster\n\r ' + str(df_point_cluster)
+        # Transpose back
+        df_dist_to_cluster_centers = df_dist_to_cluster_centers.transpose()
+        lg.Log.debugdebug(
+            'Cluster Code Book:\n\r' + str(np_cluster_labels)
+            + '\n\rDistance to Cluster Centers:\n\r' + str(df_dist_to_cluster_centers)
         )
-        return {
-            Cluster.COL_CLUSTER_NDARRY: np_cluster,
-            Cluster.COL_CLUSTER_MATRIX: df_cluster_matrix,
-            Cluster.COL_CODE_BOOK:      df_point_cluster
-        }
+
+        retobj = Cluster.ReturnCluster(
+            np_cluster_centers = np_cluster_centers,
+            np_cluster_labels  = np_cluster_labels,
+            df_distances_point_to_cc = df_dist_to_cluster_centers
+        )
+
+        #
+        # Get cluster radius &
+        #
+        df_cluster_radius = pd.DataFrame(
+            data = {
+                Cluster.COL_CLUSTER_LABEL: np_unique_cluster_labels,
+                Cluster.COL_CLUSTER_RADIUS: [0.0] * np_unique_cluster_labels.shape[0]
+            }
+        )
+        for idx in np_unique_cluster_labels:
+            # Get just this cluster points distances
+            dist_cluster = df_dist_to_cluster_centers[np_cluster_labels==idx]
+            # Just the cluster column
+            cluster_col = np.array(dist_cluster[idx])
+            # Max of the label column
+            max_dist = max(cluster_col)
+            df_cluster_radius[Cluster.COL_CLUSTER_RADIUS].at[idx] = max_dist
+
+        lg.Log.debugdebug(
+            'Cluster Radius:\n\r' + str(df_cluster_radius)
+        )
+
+        return retobj
 
 
 if __name__ == '__main__':
-    lg.Log.LOGLEVEL = lg.Log.LOG_LEVEL_DEBUG_2
+    lg.Log.LOGLEVEL = lg.Log.LOG_LEVEL_INFO
     fn = ['a', 'b', 'c', 'd', 'e']
     m = np.zeros((9, 5))
     m[0] = [1,2,1,0,0]
@@ -244,14 +288,14 @@ if __name__ == '__main__':
     print('Optimal Clusters = ' + str(optimal_clusters))
 
     retval = Cluster.cluster(
-        matx=m,
-        feature_names=fn,
-        ncenters=10,
-        iterations=20
+        matx          = m,
+        feature_names = fn,
+        ncenters      = 3,
+        iterations    = 20
     )
-    print(retval[Cluster.COL_CLUSTER_NDARRY])
-    print(retval[Cluster.COL_CLUSTER_MATRIX])
-    print(retval[Cluster.COL_CODE_BOOK])
+    print('Cluster Centers:\n\r' + str(retval.np_cluster_centers))
+    print('Cluster Labels:\n\r' + str(retval.np_cluster_labels))
+    print('Cluster Point Distances to CC:\n\r' + str(retval.df_distances_point_to_cc))
 
     fn = ['a', 'b', 'c', 'd', 'e']
     m = np.array([
@@ -267,6 +311,6 @@ if __name__ == '__main__':
         ncenters=10,
         iterations=20
     )
-    print(retval[Cluster.COL_CLUSTER_NDARRY])
-    print(retval[Cluster.COL_CLUSTER_MATRIX])
-    print(retval[Cluster.COL_CODE_BOOK])
+    print('Cluster Centers:\n\r' + str(retval.np_cluster_centers))
+    print('Cluster Labels:\n\r' + str(retval.np_cluster_labels))
+    print('Cluster Point Distances to CC:\n\r' + str(retval.df_distances_point_to_cc))
