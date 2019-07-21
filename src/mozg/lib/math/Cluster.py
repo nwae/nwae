66bc944d807be4e6cd6044378c8cb088e1440cf9
mapktah <mapktah@yandex.ru>
2019-07-21 17:46:52 +0800

    improvements

--- src/mozg/lib/math/Cluster.py
+++ src/mozg/lib/math/Cluster.py
@@ -35,12 +35,14 @@ class Cluster:
                 np_cluster_centers,
                 np_cluster_labels,
                 df_distances_point_to_cc,
-                df_cluster_radius
+                df_cluster_radius,
+                val_max_cluster_radius
         ):
             self.np_cluster_centers = np_cluster_centers
             self.np_cluster_labels = np_cluster_labels,
             self.df_distances_point_to_cc = df_distances_point_to_cc
             self.df_cluster_radius = df_cluster_radius
+            self.val_max_cluster_radius = val_max_cluster_radius
             return
 
     #
@@ -263,7 +265,8 @@ class Cluster:
             np_cluster_centers = np_cluster_centers,
             np_cluster_labels  = np_cluster_labels,
             df_distances_point_to_cc = df_dist_to_cluster_centers,
-            df_cluster_radius = df_cluster_radius
+            df_cluster_radius = df_cluster_radius,
+            val_max_cluster_radius = max(df_cluster_radius[Cluster.COL_CLUSTER_RADIUS])
         )
 
         return retobj
@@ -300,6 +303,7 @@ if __name__ == '__main__':
     print('Cluster Labels:\n\r' + str(retval.np_cluster_labels))
     print('Cluster Point Distances to CC:\n\r' + str(retval.df_distances_point_to_cc))
     print('Cluster Radius:\n\r' + str(retval.df_cluster_radius))
+    print('Max Cluster Radius:\n\r' + str(retval.val_max_cluster_radius))
 
     fn = ['a', 'b', 'c', 'd', 'e']
     m = np.array([
@@ -319,3 +323,4 @@ if __name__ == '__main__':
     print('Cluster Labels:\n\r' + str(retval.np_cluster_labels))
     print('Cluster Point Distances to CC:\n\r' + str(retval.df_distances_point_to_cc))
     print('Cluster Radius:\n\r' + str(retval.df_cluster_radius))
+    print('Max Cluster Radius:\n\r' + str(retval.val_max_cluster_radius))
