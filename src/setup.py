3383b4d28292393411da950f46dbee35220683b0
mapktah <mapktah@yandex.ru>
2019-07-28 08:19:16 +0800

    setup files

---
+++ src/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='mozg',
+    version='0.1.0',
+    packages=['mozg.lib'],
+    package_dir={'': 'src'},
+    url='',
+    license='',
+    author='mark.tan',
+    author_email='mapktah@gmail.com',
+    description='ML Algos'
+)
