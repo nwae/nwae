7a3d4f2b33d6e426ff956a8271aab998ccb4ea77
mapktah <mapktah@yandex.ru>
2019-07-30 16:27:21 +0800

    updates

--- src/setup.py
+++ src/setup.py
@@ -4,12 +4,20 @@ setup(
     name='mozg',
     version='0.1.0',
     packages=[
-        'mozg.lib.math', 'mozg.lib.math.ml', 'mozg.lib.math.ml.metricspace'
+        'mozg.lib.math',
+        'mozg.lib.math.ml',
+        'mozg.lib.math.ml.metricspace',
+        'mozg.lib.lang',
+        'mozg.lib.lang.characters',
+        'mozg.lib.lang.classification',
+        'mozg.lib.lang.model',
+        'mozg.lib.lang.nlp',
+        'mozg.lib.lang.stats'
     ],
     package_dir={'': 'src'},
     url='',
     license='',
-    author='mark.tan',
+    author='Mark',
     author_email='mapktah@gmail.com',
     description='ML Algos'
 )
