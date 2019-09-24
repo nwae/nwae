7f59034b55494e42798ad5f052e7f137765e65c5
mapktah <mapktah@yandex.ru>
2019-09-24 09:54:49 +0900

    clean training data from var and var declarations

--- src/nwae/lib/lang/nlp/daehua/DaehuaTrainDataModel.py
+++ src/nwae/lib/lang/nlp/daehua/DaehuaTrainDataModel.py
@@ -2,9 +2,9 @@
 
 import nwae.utils.Log as lg
 from inspect import getframeinfo, currentframe
-import nwae.config.Config as cf
-import nwae.lib.lang.LangFeatures  as lf
-import json
+import re
+import pandas as pd
+import nwae.utils.StringUtils as su
 
 
 #
@@ -54,17 +54,86 @@ class DaehuaTrainDataModel:
         self.daehua_training_data = daehua_training_data
         return
 
+    def remove_variables_and_variable_declarations(
+            self,
+            str_list
+    ):
+        clean_1_list = self.remove_variables(
+            var_list = str_list
+        )
+        clean_2_list = self.remove_variable_declaration(
+            decl_list = clean_1_list
+        )
+        return clean_2_list
+
+    def remove_variables(
+            self,
+            var_list
+    ):
+        try:
+            clean_list = [re.sub(pattern='[$]{2}[a-z0-9]+', repl='', string=s) for s in var_list]
+            clean_list = [re.sub(pattern='[ ]+', repl=' ', string=s) for s in clean_list]
+            clean_list = [su.StringUtils.trim(s) for s in clean_list]
+            return clean_list
+        except Exception as ex:
+            errmsg = str(self.__class__) + ' ' + str(getframeinfo(currentframe()).lineno)\
+                     + ': Exception "' + str(ex) + '" cleaning var list ' + str(var_list)
+            lg.Log.error(errmsg)
+            raise Exception(errmsg)
+
+    def remove_variable_declaration(
+            self,
+            decl_list
+    ):
+        try:
+            clean_decl_list = [re.sub(pattern='[ ]*[-*-].*[-*-]', repl='', string=s) for s in decl_list]
+            clean_decl_list = [re.sub(pattern='[ ]+', repl=' ', string=s) for s in clean_decl_list]
+            clean_decl_list = [su.StringUtils.trim(s) for s in clean_decl_list]
+            return clean_decl_list
+        except Exception as ex:
+            errmsg = str(self.__class__) + ' ' + str(getframeinfo(currentframe()).lineno)\
+                     + ': Exception "' + str(ex) + '" cleaning var declaration list ' + str(decl_list)
+            lg.Log.error(errmsg)
+            raise Exception(errmsg)
+
     #
-    # Here we decode the daehua training data language.
+    # Here we clean the daehua training data language, and leave only pure training data.
     # It can also be just plain training data of which we let it pass.
     #
-    def process_daehua_training_data(
+    def clean_daehua_training_data(
             self
     ):
-        df_daehua_processed = self.daehua_training_data
+        df_daehua_processed = pd.DataFrame(self.daehua_training_data)
         #
-        # Process by intent ID
+        # Process by intent
         #
+        # lg.Log.debugdebug(
+        #     str(self.__class__) + ' ' + str(getframeinfo(currentframe()).lineno)
+        #     + ': Training Data:\n\r' + str(df_daehua_processed.values)
+        # )
+
+        #
+        # Clean intent names
+        #
+        intent_names_list = df_daehua_processed[DaehuaTrainDataModel.COL_TDATA_INTENT_NAME].tolist()
+        df_daehua_processed[DaehuaTrainDataModel.COL_TDATA_INTENT_NAME] =\
+            self.remove_variable_declaration(decl_list = intent_names_list)
+
+        #
+        # Clean variables from txt and txt segmented
+        #
+        text_list = df_daehua_processed[DaehuaTrainDataModel.COL_TDATA_TEXT].tolist()
+        df_daehua_processed[DaehuaTrainDataModel.COL_TDATA_TEXT] =\
+            self.remove_variables_and_variable_declarations(str_list = text_list)
+
+        text_seg_list = df_daehua_processed[DaehuaTrainDataModel.COL_TDATA_TEXT_SEGMENTED].tolist()
+        df_daehua_processed[DaehuaTrainDataModel.COL_TDATA_TEXT_SEGMENTED] =\
+            self.remove_variables_and_variable_declarations(str_list = text_seg_list)
+
+        lg.Log.info(
+            str(self.__class__) + ' ' + str(getframeinfo(currentframe()).lineno)
+            + ': Training Data:\n\r' + str(df_daehua_processed.values)
+        )
 
         return df_daehua_processed
 
