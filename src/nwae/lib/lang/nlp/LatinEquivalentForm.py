908b73220c7af506fe2fc5de2bafdc5649d5fb85
mapktah <mapktah@yandex.ru>
2019-11-30 09:04:06 +0800

    missing vietnamese characters

--- src/nwae/lib/lang/nlp/LatinEquivalentForm.py
+++ src/nwae/lib/lang/nlp/LatinEquivalentForm.py
@@ -2,6 +2,7 @@
 # --*-- coding: utf-8 --*--
 
 import re
+import nwae.lib.lang.LangFeatures as lf
 
 
 #
@@ -15,34 +16,42 @@ class LatinEquivalentForm:
         return
 
     @staticmethod
-    def get_latin_equivalent_form(lang, word):
-
+    def get_latin_equivalent_form(
+            # Language is just to speed up the function so that
+            # it does not do anything if not required, you may pass None
+            lang,
+            word
+    ):
         wordlatin = word
-
         # For Vietnamese, we add a Latin column mapping (actually we can also do this for other languages)
-        if lang == 'vn':
+        if lang in [lf.LangFeatures.LANG_VI, lf.LangFeatures.LANG_VN]:
             # Map [ăâ àằầ ảẳẩ ãẵẫ áắấ ạặậ] to latin 'a', [ê èề ẻể ẽễ éế ẹệ] to 'e', [ì ỉ ĩ í ị] to 'i',
             # [ôơ òồờ ỏổở õỗỡ óốớ ọộợ] to 'o', [ư ùừ ủử ũữ úứ ụự] to u, [đ] to 'd'
-            wordlatin = re.sub(u'[ăâàằầảẳẩãẵẫáắấạặậ]', u'a', wordlatin)
-            wordlatin = re.sub(u'[ĂÂÀẰẦẢẲẨÃẴẪÁẮẤẠẶẬ]', u'A', wordlatin)
-            wordlatin = re.sub(u'[êèềẻểẽễéếẹệ]', u'e', wordlatin)
-            wordlatin = re.sub(u'[ÊÈỀẺỂẼỄÉẾẸỆ]', u'E', wordlatin)
-            wordlatin = re.sub(u'[ìỉĩíị]', u'i', wordlatin)
-            wordlatin = re.sub(u'[ÌỈĨÍỊ]', u'I', wordlatin)
-            wordlatin = re.sub(u'[ôơòồờỏổởõỗỡóốớọộợ]', u'o', wordlatin)
-            wordlatin = re.sub(u'[ÔƠÒỒỜỎỔỞÕỖỠÓỐỚỌỘỢ]', u'O', wordlatin)
-            wordlatin = re.sub(u'[ưùừủửũữúứụự]', u'u', wordlatin)
-            wordlatin = re.sub(u'[ƯÙỪỦỬŨỮÚỨỤỰ]', u'U', wordlatin)
-            wordlatin = re.sub(u'[đ]', u'd', wordlatin)
-            wordlatin = re.sub(u'[Đ]', u'D', wordlatin)
+            wordlatin = re.sub('[ăâàằầảẳẩãẵẫáắấạặậ]', 'a', wordlatin)
+            wordlatin = re.sub('[ĂÂÀẰẦẢẲẨÃẴẪÁẮẤẠẶẬ]', 'A', wordlatin)
+            wordlatin = re.sub('[êèềẻểẽễéếẹệ]', 'e', wordlatin)
+            wordlatin = re.sub('[ÊÈỀẺỂẼỄÉẾẸỆ]', 'E', wordlatin)
+            wordlatin = re.sub('[ìỉĩíị]', 'i', wordlatin)
+            wordlatin = re.sub('[ÌỈĨÍỊ]', 'I', wordlatin)
+            wordlatin = re.sub('[ôơòồờỏổởõỗỡóốớọộợ]', 'o', wordlatin)
+            wordlatin = re.sub('[ÔƠÒỒỜỎỔỞÕỖỠÓỐỚỌỘỢ]', 'O', wordlatin)
+            wordlatin = re.sub('[ưùừủửũữúứụự]', 'u', wordlatin)
+            wordlatin = re.sub('[ƯÙỪỦỬŨỮÚỨỤỰ]', 'U', wordlatin)
+            wordlatin = re.sub('[đ]', 'd', wordlatin)
+            wordlatin = re.sub('[Đ]', 'D', wordlatin)
+            wordlatin = re.sub('[ýỳỷỹỵ]', 'y', wordlatin)
         else:
             # TODO: Convert to latin equivalent for other languages
             wordlatin = word
 
         return wordlatin
 
-def demo_1():
-    return
 
-
-demo_1()
+if __name__ == '__main__':
+    lang = lf.LangFeatures.LANG_VN
+    print(LatinEquivalentForm.get_latin_equivalent_form(lang=lang, word='Anh yêu em'))
+    print(LatinEquivalentForm.get_latin_equivalent_form(lang=lang, word='đây là tiếng Latin'))
+    print(LatinEquivalentForm.get_latin_equivalent_form(lang=None, word='니는 영화를 조아'))
+    print(LatinEquivalentForm.get_latin_equivalent_form(lang=None, word='我喜欢吃点心'))
+    print(LatinEquivalentForm.get_latin_equivalent_form(lang=None, word='как дела'))
+    print(LatinEquivalentForm.get_latin_equivalent_form(lang=None, word='สวัสดี ไปไหนมา'))
