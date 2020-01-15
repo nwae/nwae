c175dd64e4a1bf3de068ffead394ea1c1bd1a02d
Mark <mapktah@ya.ru>
2020-01-15 08:42:29 +0800

    update

--- src/nwae/lib/math/ml/examples/ClassifyText.py
+++ src/nwae/lib/math/ml/examples/ClassifyText.py
@@ -4,12 +4,15 @@
 # Partly from https://learn-neural-networks.com/world-embedding-by-keras/
 #
 
-from numpy import array
+import numpy as np
+from nwae.lib.math.NumpyUtil import NumpyUtil
 import keras.preprocessing as kerasprep
 import keras.layers as keraslay
+import keras.utils as kerasutils
 from keras.models import Sequential
 from nwae.lib.lang.preprocessing.BasicPreprocessor import BasicPreprocessor
 
+BINARY_MODEL = False
 
 # Training data or Documents
 docs_label = [
@@ -18,13 +21,20 @@ docs_label = [
     ('Плохо!',0), ('Дурак!',0),
     ('나쁜!',0), ('바보!',0), ('백치!',0), ('얼간이!',0),
     ('미친놈',0), ('씨발',0), ('개',0), ('개자식',0),
-    ('젠장',0)
+    ('젠장',0),
+    ('ok',2), ('fine',2)
 ]
 docs = [x[0].split(' ') for x in docs_label]
 docs = [BasicPreprocessor.clean_punctuations(sentence=sent) for sent in docs]
 labels = [x[1] for x in docs_label]
+# How many unique labels
+n_labels = len(list(set(labels)))
+# Convert labels to categorical one-hot encoding
+labels_categorical = kerasutils.to_categorical(labels, num_classes=n_labels)
+
 print('Docs: ' + str(docs))
 print('Labels: ' + str(labels))
+print('Labels converted to categorical: ' + str(labels_categorical))
 
 unique_words = list(set([w for sent in docs for w in sent]))
 print('Unique words: ' + str(unique_words))
@@ -53,49 +63,76 @@ print(enc_docs)
 
 # pad documents to a max length of 4 words
 max_length = 1
-for sent_label in docs_label:
-    max_length = max(len(sent_label[0].split(' ')), max_length)
+for sent in enc_docs:
+    max_length = max(len(sent), max_length)
 print('Max Length = ' + str(max_length))
 
-p_docs = kerasprep.sequence.pad_sequences(enc_docs, maxlen=max_length, padding='post')
-print('Padded Encoded Sentences (' + str(len(p_docs)) + '):')
+p_docs = kerasprep.sequence.pad_sequences(enc_docs, maxlen=max_length, padding='pre')
+print('Padded Encoded Sentences (' + str(p_docs.shape) + '):')
 print(p_docs)
 
 #
 # The neural network model training
 #
 # define the model
-modelEmb = Sequential()
+model = Sequential()
 #
 # If each sentence has n words, and each word is 8 dimensions (output dim
 # of embedding layer), this means the final output of a sentence is (n,8)
 # in dimension.
 #
 embedding_layer = keraslay.embeddings.Embedding(
-        input_dim    = vs,
-        # Each word represented by a vector of dimension 8
-        output_dim   = 8,
-        input_length = max_length
-    )
+    input_dim    = vs,
+    # Each word represented by a vector of dimension 8
+    output_dim   = 8,
+    # How many words, or length of input vector
+    input_length = max_length
+)
 
 # Model
-modelEmb.add(embedding_layer)
-# After flattening each sentence of shape (4,8), will have shape (32)
-modelEmb.add(keraslay.Flatten())
-# Our standard dense layer, with 1 node
-modelEmb.add(keraslay.Dense(1, activation='sigmoid'))
-# compile the model
-modelEmb.compile(optimizer='adam', loss='binary_crossentropy', metrics=['acc'])
-# summarize the model
-print(modelEmb.summary())
-# fit the model
-modelEmb.fit(p_docs, array(labels), epochs=150, verbose=0)
+model.add(embedding_layer)
+# After flattening each sentence of shape (max_length,8), will have shape (max_length*8)
+model.add(keraslay.Flatten())
+
+if BINARY_MODEL:
+    # Our standard dense layer, with 1 node
+    model.add(keraslay.Dense(units=1, activation='sigmoid'))
+    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['acc'])
+    # summarize the model
+    print(model.summary())
+    # Train/Fit the model
+    model.fit(p_docs, np.array(labels), epochs=150, verbose=0)
+else:
+    model.add(keraslay.Dense(units=n_labels*3, activation='sigmoid'))
+    model.add(keraslay.Dense(units=n_labels, activation='softmax'))
+    model.compile(optimizer='rmsprop', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+    # summarize the model
+    print(model.summary())
+    # Train/Fit the model. Don't know why need to use 'sparse_categorical_crossentropy'
+    # and labels instead of labels_categorical
+    model.fit(p_docs, np.array(labels), epochs=150, verbose=0)
 
 #
 # Evaluate the model
 #
-loss, accuracy = modelEmb.evaluate(p_docs, array(labels), verbose=2)
+loss, accuracy = model.evaluate(p_docs, np.array(labels), verbose=2)
 print('Accuracy: %f' % (accuracy*100))
 
-probs = modelEmb.predict(p_docs)
+probs = model.predict(p_docs)
 print('Probs:' + str(probs))
+
+# Compare some data
+count_correct = 0
+for i in range(len(p_docs)):
+    data_i = np.array([p_docs[i]])
+    label_i = labels[i]
+    prob_distribution = model.predict(x=data_i)
+    top_x = NumpyUtil.get_top_indexes(
+        data      = prob_distribution[0],
+        ascending = False,
+        top_x     = 5
+    )
+    if top_x[0] == label_i:
+        count_correct += 1
+    print(str(i) + '. ' + str(data_i) + ': Label=' + str(label_i) + ', predicted=' + str(top_x))
+print('Accuracy = ' + str(100*count_correct/len(p_docs)) + '%.')
