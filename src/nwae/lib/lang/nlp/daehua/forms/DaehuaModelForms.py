21e158beb2e8cbc06a570988d229fb2d2319a093
Mark <mapktah@ya.ru>
2020-03-08 14:45:15 +0800

    cleanup form conversation

--- src/nwae/lib/lang/nlp/daehua/forms/DaehuaModelForms.py
+++ src/nwae/lib/lang/nlp/daehua/forms/DaehuaModelForms.py
@@ -10,6 +10,13 @@ from nwae.utils.StringUtils import StringUtils
 
 class DaehuaModelForms:
 
+    FORM_STATE_NONE = 'none'
+    FORM_STATE_AWAIT_FIELD_VALUE = 'awaitFieldValue'
+    FORM_STATE_AWAIT_FIELD_VALUE_CONFIRMATION = 'awaitFieldValueConfirmation'
+    FORM_STATE_FORM_COMPLETED = 'formCompleted'
+    FORM_STATE_AWAIT_FORM_CONFIRMATION = 'awaitFormConfirmation'
+    FORM_STATE_FORM_COMPLETED_AND_CONFIRMED = 'formCompletedAndConfirmed'
+
     DEFAULT_OK = ('y', 'ok', 'yes')
     def __init__(
             self,
@@ -39,32 +46,38 @@ class DaehuaModelForms:
     def get_form(self):
         return self.form
 
+    def set_daehua_form_state(
+            self,
+            state
+    ):
+        self.form_state = state
+
     def reset(self):
         Log.important('Form reset')
+        self.form_state = DaehuaModelForms.FORM_STATE_NONE
         # The current field we are trying to extract from user
         self.conv_current_field_index = None
         self.conv_current_field_name = None
         self.conv_current_field = None
-        # Field values all completed
-        self.conv_completed = False
-        # User already confirmed the form values
-        self.conv_completed_and_confirmed = False
         # Reset fields
         self.form.reset_fields_to_incomplete()
         return
 
     def __set_conversation_complete(self, caller=None):
         Log.important('Conversation set to completed by ' + str(caller))
-        self.conv_completed = True
+        self.form_state = DaehuaModelForms.FORM_STATE_FORM_COMPLETED
 
     def __set_conversation_complete_and_confirmed(self, caller=None):
         Log.important('Conversation set to completed and confirmed by ' + str(caller))
-        self.conv_completed_and_confirmed = True
+        self.form_state = DaehuaModelForms.FORM_STATE_FORM_COMPLETED_AND_CONFIRMED
 
     def get_next_question(
             self
     ):
-        if self.conv_completed:
+        if self.form_state in (
+                DaehuaModelForms.FORM_STATE_FORM_COMPLETED,
+                DaehuaModelForms.FORM_STATE_FORM_COMPLETED_AND_CONFIRMED
+        ):
             return None
 
         self.conv_current_field_index = None
@@ -82,7 +95,7 @@ class DaehuaModelForms:
 
         if self.conv_current_field_index is None:
             # Answer-Questioning completed
-            self.__set_conversation_complete(caller='get_next_question')
+            self.__set_conversation_complete(caller=str(getframeinfo(currentframe()).lineno))
             return None
 
         cur_field = self.form.form_fields[self.conv_current_field_index]
@@ -190,6 +203,35 @@ class DaehuaModelForms:
                 completed_fields.append(fld.to_json())
         return completed_fields
 
+    def simulate_question_answer(
+            self
+    ):
+        while not self.form_state == DaehuaModelForms.FORM_STATE_FORM_COMPLETED_AND_CONFIRMED:
+            if self.form_state == DaehuaModelForms.FORM_STATE_FORM_COMPLETED:
+                print('Form values completed. Asking for confirmation..')
+                self.form_state = DaehuaModelForms.FORM_STATE_AWAIT_FORM_CONFIRMATION
+
+                answer = input(self.get_confirm_form_question())
+                self.confirm_form(answer=answer)
+                continue
+
+            q = self.get_next_question()
+            if q is None:
+                self.set_daehua_form_state(state=DaehuaModelForms.FORM_STATE_FORM_COMPLETED)
+            else:
+                self.set_daehua_form_state(state=DaehuaModelForms.FORM_STATE_AWAIT_FIELD_VALUE)
+
+                answer = input(q + '\n\r')
+                print('User answer: ' + str(answer))
+
+                (value, confirm_question) = self.extract_param_value(
+                    answer=answer
+                )
+                if value is not None:
+                    self.confirm_current_field()
+                    # answer = input(confirm_question)
+                    # fconv.confirm_answer(answer=answer)
+
 
 if __name__ == '__main__':
     colform = {
@@ -219,24 +261,4 @@ if __name__ == '__main__':
         form = dform
     )
 
-    while not fconv.conv_completed_and_confirmed:
-        print('Form complete status: ' + str(fconv.conv_completed))
-        if fconv.conv_completed:
-            print('Form values completed. Asking for confirmation..')
-            answer = input(fconv.get_confirm_form_question())
-            fconv.confirm_form(answer=answer)
-            continue
-
-        q = fconv.get_next_question()
-        if q is None:
-            continue
-        answer = input(q + '\n\r')
-        print('User answer: ' + str(answer))
-
-        (value, confirm_question) = fconv.extract_param_value(
-            answer = answer
-        )
-        if value is not None:
-            fconv.confirm_current_field()
-            #answer = input(confirm_question)
-            #fconv.confirm_answer(answer=answer)
+    fconv.simulate_question_answer()
